070ae15cd5576058bc1f4aa1908b3664c189be71
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-01-30 20:09:28 -0800

    fetching notes first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import fetchUser from './fetchUser';
+import fetchData from './fetchData';
 import UserHeader from './UserHeader';
 import './App.css';
 
 function App() {
 
   const [user, setUser] = useState({});
-  const [notes, setNotes] = useState([]);
-  const [vacations, setVacations] = useState([]);
+  const [notes, setNotes] = useState([{id:'123',text:'words'}]);
+  //const [vacations, setVacations] = useState([]);
+  //const [companies, setCompanies] = useState([]);
 
   const newUser = ()=>{
     fetchUser().then(res=>setUser(res));
@@ -20,13 +22,28 @@ function App() {
     console.log(user);
   }, [user])
 
+  useEffect( ()=>{
+    
+    if(user.id !== 'undefined'){
+      console.log(`fetching from userId ${user.Id}`);
+      //fetchData(user.id).then(res=>setNotes(res));
+    }
+  },[user])
+
   return (
     <div className="App">
       <UserHeader user = {user} newUser = {newUser}/>
+      <div>{user.id}</div>
+      <div className = 'notes'>
+        <h2> Notes Section </h2>
+        {
+         notes.map((note, idx)=> <div key = {idx}> Hello</div>)
+        }
+
+      </div>
       
 
-      
-      
+  
     </div>
   );
 }
--- src/fetchData.js
+++ src/fetchData.js
@@ -1,4 +1,12 @@
-const fetchData = () => {
+import axios from 'axios';
+
+const fetchData = async (userId) => {
+    console.log(`fetching from userId ${userId}`);
+    const API = `https://acme-users-api-rev.herokuapp.com/api/users/${userId}/notes`;
+    
+    const notes = await axios.get(API)
+        .then(res=>console.log(res.data))
+        .catch(ex=>console.log(ex));
 
 }
 
