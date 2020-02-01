c2a68aa450448962584d0c498a912d1fd35ae884
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-02-01 11:43:07 -0800

    added post & delete function, cleaning up date formatting

--- src/Vacations.js
+++ src/Vacations.js
@@ -1,12 +1,26 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import moment from 'moment';
 
-const Vacations = ({vacations}) => {
-    const [vac, setVac] = useState({start: '', end: ''});
-
+const Vacations = ({vacations, setVacations, userId}) => {
+    const [vac, setVac] = useState({startDate: '', endDate: ''});
+    const API = `https://acme-users-api-rev.herokuapp.com/api/users/${userId}/vacations`;
+    
     const submit = (ev) => {
         ev.preventDefault();
-        console.log(vac)
+        axios.post(API, vac)
+        .then(res => {
+            console.log(res);
+            setVacations([res,...vacations]);
+        })
+        .catch(ex => console.log(ex))
+    }
+
+    const deleteVacation = (vacId) => {
+        axios.delete(`${API}/${vacId}`)
+        .then(res => console.log(res))
+        const newVac = vacations.filter(vac => vac.id !== vacId);
+        setVacations(newVac);
     }
 
     return (
@@ -18,28 +32,32 @@ const Vacations = ({vacations}) => {
                         <form onSubmit = {(ev)=> submit(ev)}>
                             <label> Start Date </label>
                             <input type = 'text' 
-                                value = {vac.start}
-                                onChange = { ev => setVac({...vac, start: ev.target.value})} />
+                                value = {vac.startDate}
+                                onChange = { ev => setVac({...vac, startDate: ev.target.value})} />
                             <br/>
                             <label> End Date </label>
                             <input type = 'text' 
-                                value = {vac.end}
-                                onChange = { ev => setVac({...vac, end: ev.target.value})} />
+                                value = {vac.endDate}
+                                onChange = { ev => setVac({...vac, endDate: ev.target.value})} />
                             <br/>
                             <button>Submit</button>
                         </form>
                         <p>Max Limit of 3 Vacations.</p>
                     </div>
-                {
-                vacations.map( vacation => {
-                    return (
-                        <div key ={vacation.id} className = 'note-card'>
-                            <div> Start Date: {moment(vacation.startDate).format('dddd MM/DD/YYYY')} </div>
-                            <div> End Date: {moment(vacation.endDate).format('dddd MM/DD/YYYY')} </div>
-                            <div> {moment(vacation.endDate).diff(moment(vacation.startDate),'days')} Days long</div> 
-                        </div>
-                    )})
-                }    
+                    <div className = 'container-notes'>
+                        {
+                        vacations.map( vacation => {
+                            return (
+                                <div key ={vacation.id} className = 'note-card'>
+                                    <div> Start Date: {moment(vacation.startDate).format('dddd MM/DD/YYYY')} </div>
+                                    <div> End Date: {moment(vacation.endDate).format('dddd MM/DD/YYYY')} </div>
+                                    <div> {moment(vacation.endDate).diff(moment(vacation.startDate),'days')} Days long</div>
+                                    <button onClick = {()=>deleteVacation(vacation.id)}> Delete this Vacation</button> 
+                                </div>
+                            )})
+                        }
+                    </div>
+                    
             </div>
         </div>
     )
