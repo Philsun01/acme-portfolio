5a0e422ed9cd2890afbe42d51b3d7583094c9f4b
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-02-01 10:37:30 -0800

    creating note submit form

--- src/Notes.js
+++ src/Notes.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Notes = ({notes}) => {
+const Notes = ({notes, userId}) => {
+
+    const API = `https://acme-users-api-rev.herokuapp.com/api/users/${userId}/notes`;
+    const [text, setText] = useState('');
+    
+    const submit = (ev) => {
+        ev.preventDefault();
+        console.log('submitted');
+    }
 
     return (
         <div>
             <h1> Notes Here</h1>
-            {
+            <div className = 'container-sub'>
+                <div className = 'add-note'>
+                    <h2> Add Note</h2>
+                    <form onSubmit = {()=> submit()}>
+                        <input id = 'add-note-form' type = 'text' 
+                            value = {text}
+                            onChange = { ev => setText(ev.target.value)} />
+                        <br/>
+                        <button>Submit</button>
+                    </form>
+                </div>
+                <div className = 'container-notes'>
+                {
                 notes.map( note => {
-                    return (<div key = {note.id}>
+                    return (
+                    <div key = {note.id}
+                        className = 'note-card'>
                         {note.text}
+                        <button onClick = {() => console.log(note.id)}>
+                            Delete Note
+                        </button>
                     </div>)
                 })
-            }
+                }
+                </div>
+            </div>
             
 
         </div>
